454f8a77b598fc0e6a9b0bb7eaaa0db4636ee3e6
lordeiro-dias <eduborgesbldias@gmail.com>
2026-03-05 19:23:58 -0300

    :sparkles: Genero Completo

---
+++ Applications/Services/GeneroService.cs
@@ -0,0 +1,105 @@
+﻿using RoyalGames.Domains;
+using RoyalGames.DTOs.GeneroDto;
+using RoyalGames.Exceptions;
+using RoyalGames.Interfaces;
+
+namespace RoyalGames.Applications.Services
+{
+    public class GeneroService
+    {
+        private readonly IGeneroRepository _repository;
+
+        public GeneroService(IGeneroRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<LerGeneroDto> Listar()
+        {
+            List<Genero> generos = _repository.Listar();
+
+            List<LerGeneroDto> generoDto = generos.Select(g => new LerGeneroDto
+            {
+                GeneroID = g.GeneroID,
+                Nome = g.Gênero
+            }).ToList();
+
+            return generoDto;
+        }
+
+        public LerGeneroDto ObterPorId(int id)
+        {
+            Genero genero = _repository.ObterPorId(id);
+
+            if(genero == null)
+            {
+                throw new DomainException("Gênero não encontrado.");
+            }
+
+            LerGeneroDto generoDto = new LerGeneroDto
+            {
+                GeneroID = genero.GeneroID,
+                Nome = genero.Gênero
+            };
+
+            return generoDto;
+        }
+
+        private static void ValidarNome(string nome)
+        {
+            if(string.IsNullOrEmpty(nome))
+            {
+                throw new DomainException("Nome é obrigatório.");
+            }
+        }
+
+        public void Adicionar(CriarGeneroDto criarDto)
+        {
+            ValidarNome(criarDto.Nome);
+
+            if(_repository.NomeExiste(criarDto.Nome))
+            {
+                throw new DomainException("Gênnero já existente.");
+            }
+
+            Genero genero = new Genero
+            {
+                Gênero = criarDto.Nome
+            };
+
+            _repository.Adicionar(genero);
+        }
+
+        public void Atualizar(int id, CriarGeneroDto criarDto)
+        {
+            ValidarNome(criarDto.Nome);
+
+            Genero generoBanco = _repository.ObterPorId(id);
+
+            if(generoBanco == null)
+            {
+                throw new DomainException("Gênero não foi encontrado");
+            }
+
+            if(_repository.NomeExiste(criarDto.Nome, generoIdAtual: id))
+            {
+                throw new DomainException("Já existe outra categoria com esse nome.");
+            }
+
+            generoBanco.Gênero = criarDto.Nome;
+            _repository.Atualizar(generoBanco);
+        }
+
+        public void Remover(int id)
+        {
+            Genero generoBanco = _repository.ObterPorId(id);
+
+            if(generoBanco == null)
+            {
+                throw new DomainException("Gênero não encontrado.");
+            }
+
+            _repository.Remover(id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RoyalGames.Applications.Autenticacao;
 using RoyalGames.Applications.Services;
 using RoyalGames.Contexts;
 using RoyalGames.Interfaces;
 using RoyalGames.Repositories;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +16,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Value: Bearer TokenJWT"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 // chamar conexão com o banco
 builder.Services.AddDbContext<Royal_GamesContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
@@ -23,6 +53,65 @@ builder.Services.AddScoped<UsuarioService>();
 // jogo
 builder.Services.AddScoped<IJogoRepository, JogoRepository>();
 builder.Services.AddScoped<JogoService>();
+
+// genero
+builder.Services.AddScoped<IGeneroRepository, GeneroRepository>();
+builder.Services.AddScoped<GeneroService>();
+
+// JWT
+builder.Services.AddScoped<GeradorTokenJWT>();
+builder.Services.AddScoped<AutenticacaoService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+    // Adiciona o suporte para autenticação usando JWT.
+    .AddJwtBearer(options =>
+    {
+        // Lê a chave secreta definida no appsettings.json.
+        // Essa chave é usada para ASSINAR o token quando ele é gerado
+        // e também para VALIDAR se o token recebido é verdadeiro.
+        var chave = builder.Configuration["Jwt:Key"]!;
+
+        // Quem emitiu o token (ex: nome da sua aplicação).
+        // Serve para evitar aceitar tokens de outro sistema.
+        var issuer = builder.Configuration["Jwt:Issuer"]!;
+
+        // Para quem o token foi criado (normalmente o frontend ou a própria API).
+        // Também ajuda a garantir que o token pertence ao seu sistema.
+        var audience = builder.Configuration["Jwt:Audience"]!;
+
+        // Define as regras que serão usadas para validar o token recebido.
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // Verifica se o emissor do token é válido
+            // (se bate com o issuer configurado).
+            ValidateIssuer = true,
+
+            // Verifica se o destinatário do token é válido
+            // (se bate com o audience configurado).
+            ValidateAudience = true,
+
+            // Verifica se o token ainda está dentro do prazo de validade.
+            // Se já expirou, a requisição será negada.
+            ValidateLifetime = true,
+
+            // Verifica se a assinatura do token é válida.
+            // Isso garante que o token não foi alterado.
+            ValidateIssuerSigningKey = true,
+
+            // Define qual emissor é considerado válido.
+            ValidIssuer = issuer,
+
+            // Define qual audience é considerado válido.
+            ValidAudience = audience,
+
+            // Define qual chave será usada para validar a assinatura do token.
+            // A mesma chave usada na geração do JWT deve estar aqui.
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(chave)
+            )
+        };
+    });
 
 var app = builder.Build();
 
