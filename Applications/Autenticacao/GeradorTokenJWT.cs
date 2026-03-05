e55298f037bac06bb5b2ab1a86a5c08df20cd330
lordeiro-dias <eduborgesbldias@gmail.com>
2026-03-05 15:56:48 -0300

    :sparkles: Autenticação completa

---
+++ Applications/Autenticacao/GeradorTokenJWT.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RoyalGames.Domains;
+using RoyalGames.Exceptions;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RoyalGames.Applications.Autenticacao
+{
+    public class GeradorTokenJWT
+    {
+        private readonly IConfiguration _config;
+
+        public GeradorTokenJWT(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GerarToken(Usuario usuario)
+        {
+            var chave = _config["Jwt:Key"]!;
+
+            var issuer = _config["Jwt:Issuer"]!;
+
+            var audience = _config["Jwt:Audience"]!;
+
+            var expiraEmMinutos = int.Parse(_config["Jwt:ExpiraEmMinutos"]!);
+
+            var keyBytes = Encoding.UTF8.GetBytes(chave);
+
+            if(keyBytes.Length < 32)
+            {
+                throw new DomainException("Jwt: Key precisa ter pelo menos 32 caracteres");
+            }
+
+            var securityKey = new SymmetricSecurityKey(keyBytes);
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, usuario.UsuarioID.ToString()),
+
+                new Claim(ClaimTypes.Name, usuario.Nome),
+
+                new Claim(ClaimTypes.Email, usuario.Email)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(expiraEmMinutos),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
